fa7a2e9afc9f94b0015081f07e9ff75d6c966dab
stephane soulier <stephane@soolier.com>
2015-06-04 11:32:32 +0200

    Add a way to select the type of center into the shiny app

--- R/shiny/ui.R
+++ R/shiny/ui.R
@@ -1,5 +1,33 @@
 library(shinydashboard)
+library(leaflet)
 
-ui <- fluidPage(
-  leafletOutput("mymap")
+header <- dashboardHeader(
+  title = "Croix Rouge"
+)
+
+body <- dashboardBody(
+  fluidRow(
+    column(width = 9,
+           box(width = NULL, solidHeader = TRUE,
+               leafletOutput("map", height = 500)
+           )
+    ),
+    column(width = 3,
+           box(width = NULL, status = "warning",
+               uiOutput("centerSelect"),
+               selectInput(inputId = "centerAction",
+                           label = "Type de centre",
+                           selected  = "Centre de Distribution Alimentaire",
+                           choices = centerAction,
+                           multiple = T)
+               ),
+               actionButton("refresh", "Recalcul")
+        )
+    )
+)
+
+dashboardPage(
+  header,
+  dashboardSidebar(disable = TRUE),
+  body
 )
